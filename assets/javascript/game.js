d6978ac4c75feade535144e5212407d33ae397c4
Dan Proctor <dproc96@gmail.com>
2019-06-02 19:52:34 -0400

    Remade the javascript as an object

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,60 +1,63 @@
-var tvShows = ['game of thrones', 'spongebob squarepants', 'friends', 'lost', 'doctor who', 'brooklyn nine nine'];
+var tvScreen = $('.tv');
+var wrongLettersDiv = $('#wrong-letters');
+var phraseDiv = $('#phrase');
 
-function generateHangman() {
-    $(".tv").attr("class", "tv");
-    var alphabet = ("abcdefghijklmnopqrstuvwxyz").split("");
-    var characterLog = {};
-    var correctLetters = [];
-    let wrongGuessCount = 0;
-    $("#wrong-letters").empty();
-    for (let letter of alphabet) {
-        $("#wrong-letters").append(`<span class="hidden" id="${letter}">${letter}</span>`);
-        characterLog[letter] = 0;
-    }
-    let randomIndex = Math.floor(Math.random() * tvShows.length);
-    let activeShow = tvShows[randomIndex];
-    let words = activeShow.split(" ");
-    let wordsByCharacter = words.map(x => x.split(""));
-    console.log(wordsByCharacter)
-    $("#phrase").empty();
-    let wordCount = 1;
-    for (let word of wordsByCharacter) {
-        $("#phrase").append(`<div id="word-${wordCount}"></div>`)
-        for (let character of word) {
-            $(`#word-${wordCount}`).append(`<span class="${character}">_ </span>`);
-            characterLog[character] = 1;
-            if(correctLetters.indexOf(character) === -1) {
-                correctLetters.push(character)
+var gameCode = {
+    tvShowList: ['game of thrones', 'spongebob squarepants', 'friends', 'lost', 'doctor who', 'brooklyn nine nine', 'community', 'the simpsons', 'cheers', 'frasier', 'the office', 'the x files', 'how i met your mother', 'the good place', 'arrow'],
+    correctLetters : [],
+    wrongLetters : [],
+    wrongGuessCount : 0,
+    generateHangman : function() {
+        for (let i = 1; i < 7; i++) {
+            $(`#part-${i}`).attr("class", "hidden");
+        }
+        tvScreen.attr("class", "tv")
+        gameCode.wrongGuessCount = 0;
+        gameCode.correctLetters = [];
+        gameCode.wrongLetters = 'abcdefghijklmnopqrstuvwxyz'.split("");
+        phraseDiv.empty();
+        wrongLettersDiv.empty();
+        let phraseToGuess = gameCode.tvShowList[Math.floor(Math.random() * gameCode.tvShowList.length)];
+        let phraseArray = phraseToGuess.split(" ").map(x => x.split(""));
+        for (let word of phraseArray) {
+            for (let character of word) {
+                phraseDiv.append(`<span class="${character}">_</span>`);
+                if (gameCode.correctLetters.indexOf(character) === -1) {
+                    gameCode.correctLetters.push(character);
+                }
+            }
+            phraseDiv.append("<span>  </span>")
+        }
+        gameCode.wrongLetters = gameCode.wrongLetters.filter(x => gameCode.correctLetters.indexOf(x) == -1);
+    },
+    checkInput : function(input) {
+        let correctIndex = gameCode.correctLetters.indexOf(input);
+        let wrongIndex = gameCode.wrongLetters.indexOf(input)
+        if (correctIndex != -1) {
+            $(`.${input}`).html(input);
+            gameCode.correctLetters.splice(correctIndex, 1);
+            if (gameCode.correctLetters.length === 0) {
+                wrongLettersDiv.html("You Win!");
+                phraseDiv.html("Press the Power Button to play again!");
             }
         }
-        wordCount++
-    }
-    $(document).on("keypress", function(event) {
-        $(`.${event.key}`).html(`${event.key}`)
-        if (correctLetters.indexOf(event.key) > -1) {
-            correctLetters.splice(correctLetters.indexOf(event.key), 1);
-            if (correctLetters.length === 0) {
-                alert("You win!")
-                $("#hangman img").attr("class","hidden");
-                $("#wrong-letters").empty();
-                $("#phrase").empty();
-                $(".tv").attr("class", "tv off");
-                wrongGuessCount = 0
+        else if (wrongIndex != -1) {
+            wrongLettersDiv.append(input);
+            gameCode.wrongLetters.splice(wrongIndex, 1);
+            gameCode.wrongGuessCount++;
+            $(`#part-${gameCode.wrongGuessCount}`).attr("class", "");
+            if (gameCode.wrongGuessCount === 6) {
+                wrongLettersDiv.html("You Lose!");
+                phraseDiv.html("Press the Power Button to play again!");
             }
         }
-        if (characterLog[event.key] === 0) {
-            $(`#${event.key}`).attr("class","none");
-            characterLog[event.key] = 1;
-            wrongGuessCount++;
-            $(`#part-${wrongGuessCount}`).attr("class", "")
-        }
-        if (wrongGuessCount === 7) {
-            alert("You lose!")
-            $("#hangman img").attr("class","hidden");
-            $("#wrong-letters").empty();
-            $("#phrase").empty();
-            $(".tv").attr("class", "tv off");
-            wrongGuessCount = 0
-        }
-    })
+    }
 }
+
+$(document).on("keypress", function(event) {
+    gameCode.checkInput(event.key);
+})
+
+$("#generate").on("click", function() {
+    gameCode.generateHangman();
+})
